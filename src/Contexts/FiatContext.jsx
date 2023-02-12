d454a32cb3655bda149fc6d2e9119957d588b969
FerencFarkas22 <85436006+FerencFarkas22@users.noreply.github.com>
2023-02-12 12:16:14 +0100

    context value changed

--- src/Contexts/FiatContext.jsx
+++ src/Contexts/FiatContext.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 export const FiatContextDefaults = {
-    value: [],
+    value: {
+        name: 'USD',
+        rate: 1,
+        symbol: '$'
+    },
     setValue: ()=>{}
 }
 
