ec79de66343288bc615c788e0167ef953138b394
FerencFarkas22 <85436006+FerencFarkas22@users.noreply.github.com>
2023-02-12 12:14:43 +0100

    Navbar mobile menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom"
 import "../Styles/Header.scss"
-
+import 'boxicons'
 
 const Header = ({ menu, children }) => {
+    const [active, setActive] = useState(false);
 
+    function classSetter() {
+        active === false ? setActive(true) : setActive(false)
+    }
+    function linkSetter() {
+        setActive(false)
+    }
 
     return (
         <div className="Header">
             <div className="leftSideLogo">
                 <h2>LOGO</h2>
             </div>
-            <div className="rightSideDesktopLinks">
-            {menu.filter(e => e.menubar === true)
-            .map(e => <Link key={e.name} to={e.path}>{e.name}</Link>)}
+            <div className={active === false ? "rightSideDesktopLinks" : "rightSideMobileLinks"}>
+                <div className="searcher">{children}</div>
+                {menu.filter(e => e.menubar === true)
+                    .map(e => <Link onClick={linkSetter} key={e.name} to={e.path}>{e.name}</Link>)}
             </div>
-            <div className="searcher">{children}</div>
+
+            <div className="navIcon">
+                <box-icon onClick={() => classSetter()} className="hamburgerIcon" size="lg" name='menu' color='#ffffff' ></box-icon>
+            </div>
         </div>
     )
 }
